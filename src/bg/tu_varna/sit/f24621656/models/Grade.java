57595afa1bafc72d8608fd13feba053f077036b6
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-17 20:42:05 +0300

    Update Grade.java - adding fields and getters

--- src/bg/tu_varna/sit/f24621656/models/Grade.java
+++ src/bg/tu_varna/sit/f24621656/models/Grade.java
@@ -1,4 +1,21 @@
 package bg.tu_varna.sit.f24621656.models;
 
+import bg.tu_varna.sit.f24621656.enums.DisciplineType;
+
 public class Grade {
+    private final Discipline name;
+    private final double value;
+
+    public Grade(Discipline name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Discipline getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
