179ca19b312ace9b531ad2964c88a0fbdd024b50
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-17 20:31:13 +0300

    Update Discipline.java - adding fields

--- src/bg/tu_varna/sit/f24621656/models/Discipline.java
+++ src/bg/tu_varna/sit/f24621656/models/Discipline.java
@@ -1,4 +1,22 @@
 package bg.tu_varna.sit.f24621656.models;
 
+import bg.tu_varna.sit.f24621656.enums.DisciplineType;
+import bg.tu_varna.sit.f24621656.enums.StudentStatus;
+
 public class Discipline {
+    private final String name;
+    private final DisciplineType type;
+
+    public Discipline(String name, DisciplineType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DisciplineType getType() {
+        return type;
+    }
 }
