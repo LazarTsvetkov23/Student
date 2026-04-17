711d5b18be752c27a10a8139508aa82ff95f6b27
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-17 21:07:47 +0300

    Update Specialty.java - adding fields, getters, setters.

--- src/bg/tu_varna/sit/f24621656/models/Specialty.java
+++ src/bg/tu_varna/sit/f24621656/models/Specialty.java
@@ -6,10 +6,18 @@ import java.util.List;
 public class Specialty {
     private final String name;
     private final List<Discipline> disciplines;
+    private int minElectiveCredits;
 
     public Specialty(String name) {
         this.name = name;
         this.disciplines = new ArrayList<>();
+        this.minElectiveCredits = 0;
+    }
+
+    public Specialty(String name, int minElectiveCredits) {
+        this.name = name;
+        this.disciplines = new ArrayList<>();
+        this.minElectiveCredits = minElectiveCredits;
     }
 
     public String getName() {
@@ -18,6 +26,14 @@ public class Specialty {
 
     public List<Discipline> getDisciplines() {
         return disciplines;
+    }
+
+    public int getMinElectiveCredits() {
+        return minElectiveCredits;
+    }
+
+    public void setMinElectiveCredits(int minElectiveCredits) {
+        this.minElectiveCredits = minElectiveCredits;
     }
 
     public void addDiscipline(Discipline discipline) {
