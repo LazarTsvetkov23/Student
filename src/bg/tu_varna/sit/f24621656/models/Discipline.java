6d4540710f7b7aa71ccb8f10c1e266b0826aea7f
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-25 20:32:59 +0300

    Update Discipline.java file

--- src/bg/tu_varna/sit/f24621656/models/Discipline.java
+++ src/bg/tu_varna/sit/f24621656/models/Discipline.java
@@ -51,12 +51,18 @@ public class Discipline {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Discipline that = (Discipline) o;
         return Objects.equals(name, that.name);
     }
 
     @Override
-    public int hashCode() { return Objects.hash(name); }
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
