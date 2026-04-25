e3165873e5897e02e162551a749dbfa31b76712b
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-25 20:33:09 +0300

    Update Specialty.java file

--- src/bg/tu_varna/sit/f24621656/models/Specialty.java
+++ src/bg/tu_varna/sit/f24621656/models/Specialty.java
@@ -37,19 +37,27 @@ public class Specialty {
 
     public Discipline findDisciplineByName(String name) {
         for(Discipline discipline : disciplines) {
-            if(discipline.getName().equalsIgnoreCase(name)) return discipline;
+            if(discipline.getName().equalsIgnoreCase(name)) {
+                return discipline;
+            }
         }
         return null;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Specialty that = (Specialty) o;
         return Objects.equals(name, that.name);
     }
 
     @Override
-    public int hashCode() { return Objects.hash(name); }
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
