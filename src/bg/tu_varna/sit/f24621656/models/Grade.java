9ac51708d6c41b6b1ec3400ecfc216c65e5fe758
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-25 19:45:16 +0300

    Update Grade.java file

--- src/bg/tu_varna/sit/f24621656/models/Grade.java
+++ src/bg/tu_varna/sit/f24621656/models/Grade.java
@@ -1,16 +1,16 @@
 package bg.tu_varna.sit.f24621656.models;
 
 public class Grade {
-    private final Discipline name;
+    private final Discipline disciplineName;
     private final double value;
 
-    public Grade(Discipline name, double value) {
-        this.name = name;
+    public Grade(Discipline disciplineName, double value) {
+        this.disciplineName = disciplineName;
         this.value = value;
     }
 
-    public Discipline getName() {
-        return name;
+    public Discipline getDisciplineName() {
+        return disciplineName;
     }
 
     public double getValue() {
