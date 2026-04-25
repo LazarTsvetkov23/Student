0ebc1bf9c295c4c97d929a61fc7a43e77b6b6dfe
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-25 20:37:28 +0300

    Creating DataRepository.java interface with all necessary methods!

---
+++ src/bg/tu_varna/sit/f24621656/contracts/DataRepository.java
@@ -0,0 +1,26 @@
+package bg.tu_varna.sit.f24621656.contracts;
+
+import bg.tu_varna.sit.f24621656.models.Discipline;
+import bg.tu_varna.sit.f24621656.models.Specialty;
+import bg.tu_varna.sit.f24621656.models.Student;
+
+import java.util.List;
+
+public interface DataRepository {
+    void addStudent(Student student);
+    Student findStudentByFacultyNumber(String fn);
+    List<Student> getAllStudents();
+    List<Student> getStudentsBySpecialtyAndCourse(String specialtyName, int year);
+    List<Student> getStudentsForProtocol(Discipline discipline);
+
+    void addSpecialty(Specialty specialty);
+    Specialty findSpecialtyByName(String name);
+    List<Specialty> getAllSpecialties();
+
+    void addDiscipline(Discipline discipline);
+    Discipline findDisciplineByName(String name);
+    List<Discipline> getAllDisciplines();
+
+    void clear();
+    boolean isEmpty();
+}
