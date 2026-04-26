0727f76a614a8fc94899d29f0f0fc7994341eac7
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-26 15:58:25 +0300

    Updating Grade.java class

--- src/bg/tu_varna/sit/f24621656/models/Grade.java
+++ src/bg/tu_varna/sit/f24621656/models/Grade.java
@@ -1,22 +1,44 @@
 package bg.tu_varna.sit.f24621656.models;
 
+/**
+ * Клас, представящ оценка.
+ *
+ * <p>Всяка оценка е свързана с дисциплина и има числена стойност
+ * в интервала [2.00, 6.00].
+ *
+ * @author Student
+ * @version 1.0
+ */
 public class Grade {
     private final Discipline disciplineName;
     private final double value;
 
-    public Grade(Discipline disciplineName, double value) {
-        this.disciplineName = disciplineName;
+    /**
+     * Конструктор за оценка.
+     *
+     * @param discipline дисциплината, по която е оценката
+     * @param value числена стойност на оценката (2.00 - 6.00)
+     */
+    public Grade(Discipline discipline, double value) {
+        this.disciplineName = discipline;
         this.value = value;
     }
 
+    /** @return дисциплината */
     public Discipline getDisciplineName() {
         return disciplineName;
     }
 
+    /** @return числената стойност на оценката */
     public double getValue() {
         return value;
     }
 
+    /**
+     * Проверява дали оценката е успешна (>= 3.00).
+     *
+     * @return true ако оценката е >= 3.00
+     */
     public boolean isPassed() {
         return value >= 3.00;
     }
