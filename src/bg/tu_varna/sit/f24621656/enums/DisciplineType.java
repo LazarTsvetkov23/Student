19c3c7edcfcdcc3830ae97c971ae781d0bb0b6c8
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-27 11:32:32 +0300

    Updating StudentStatus & DisciplineType enums

--- src/bg/tu_varna/sit/f24621656/enums/DisciplineType.java
+++ src/bg/tu_varna/sit/f24621656/enums/DisciplineType.java
@@ -1,6 +1,15 @@
 package bg.tu_varna.sit.f24621656.enums;
 
+/**
+ * Тип на учебна дисциплина.
+ *
+ * @author Student
+ * @version 1.0
+ */
 public enum DisciplineType {
+    /** Задължителна дисциплина */
     MANDATORY,
+
+    /** Избираема дисциплина */
     ELECTIVE
 }
--- src/bg/tu_varna/sit/f24621656/enums/StudentStatus.java
+++ src/bg/tu_varna/sit/f24621656/enums/StudentStatus.java
@@ -1,7 +1,18 @@
 package bg.tu_varna.sit.f24621656.enums;
 
+/**
+ * Статус на студент.
+ *
+ * @author Student
+ * @version 1.0
+ */
 public enum StudentStatus {
+    /** Записан - активен студент */
     ENROLLED,
+
+    /** Прекъснал - временно прекъснал обучението */
     INTERRUPTED,
+
+    /** Завършил - успешно завършил обучението */
     GRADUATED
 }
