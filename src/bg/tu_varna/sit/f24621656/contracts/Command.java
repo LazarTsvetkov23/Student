9bde870106fa0b4560e4693626fbb553d19e5b10
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-27 11:37:32 +0300

    Create Command.java interface with methods.

--- src/bg/tu_varna/sit/f24621656/contracts/Command.java
+++ src/bg/tu_varna/sit/f24621656/contracts/Command.java
@@ -1,8 +1,44 @@
 package bg.tu_varna.sit.f24621656.contracts;
 
+import bg.tu_varna.sit.f24621656.commands.CommandResult;
+
+/**
+ * Интерфейс за всички команди в системата.
+ *
+ * <p>Този интерфейс дефинира контракта, който всяка команда трябва да спазва.
+ * Спазва принципа Interface Segregation - интерфейсът е малък и фокусиран.
+ *
+ * @author Student
+ * @version 1.0
+ */
 public interface Command {
-    void execute(String[] args) throws IllegalArgumentException;
+
+    /**
+     * Изпълнява командата с подадените аргументи.
+     *
+     * @param args масив от аргументи (първият е името на командата)
+     * @return резултат от изпълнението на командата
+     */
+    CommandResult execute(String[] args);
+
+    /**
+     * Връща синтаксиса за употреба на командата.
+     *
+     * @return низ с формата на употреба (напр. "open &lt;file&gt;")
+     */
     String getUsage();
+
+    /**
+     * Връща кратко описание на командата.
+     *
+     * @return описание на командата
+     */
     String getDescription();
+
+    /**
+     * Връща името на командата.
+     *
+     * @return име на командата (напр. "open", "close")
+     */
     String getName();
 }
