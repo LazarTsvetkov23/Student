240f9202f32fa4fa1ca2c7f8578f9f0866d17605
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-04-29 17:16:33 +0300

    Update StudentStatus.java and Discipline.java enums

--- src/bg/tu_varna/sit/f24621656/enums/StudentStatus.java
+++ src/bg/tu_varna/sit/f24621656/enums/StudentStatus.java
@@ -1,18 +1,7 @@
 package bg.tu_varna.sit.f24621656.enums;
 
-/**
- * Статус на студент.
- *
- * @author Student
- * @version 1.0
- */
 public enum StudentStatus {
-    /** Записан - активен студент */
     ENROLLED,
-
-    /** Прекъснал - временно прекъснал обучението */
     INTERRUPTED,
-
-    /** Завършил - успешно завършил обучението */
     GRADUATED
 }
