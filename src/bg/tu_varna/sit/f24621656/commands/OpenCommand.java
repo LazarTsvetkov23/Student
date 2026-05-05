96b5c7105bbb95ae94b67ee639081e67d002b725
LazarTsvetkov23 <lazar.tsvetkov@hotmail.com>
2026-05-06 00:38:53 +0300

    Update OpenCommand.java

--- src/bg/tu_varna/sit/f24621656/commands/OpenCommand.java
+++ src/bg/tu_varna/sit/f24621656/commands/OpenCommand.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+//WORKED
+
 public class OpenCommand extends BaseCommand {
     public OpenCommand(Session session) {
         super(session);
